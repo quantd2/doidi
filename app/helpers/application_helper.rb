d23084db4f60d2cd18b064d505fec2cf82c28997
quantd2 <dinhquan.tran@gmail.com>
2017-08-20 14:57:34 +0700

    about

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,19 +8,6 @@ module ApplicationHelper
     end
   end
 
-  def link_to_add_fields(name, f, association)
-    new_object = f.object.send(association).klass.new
-    id = new_object.object_id
-    fields = f.simple_fields_for(association, new_object, child_index: id) do |builder|
-      render(association.to_s.singularize + "_fields", f: builder)
-    end
-    link_to(name, '#', class: "add_fields btn btn-primary input-button", data: {id: id, fields: fields.gsub("\n", "")})
-  end
-
-  def path_send *arg
-    Rails.application.routes.url_helpers.send *arg
-  end
-
   def admin?
     current_user.admin ? true : false
   end
