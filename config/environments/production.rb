971d16dd427fdd4281f6632208fdbad3a1e9abf3
quantd2 <dinhquan.tran@gmail.com>
2017-08-24 22:37:09 +0700

    add email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,21 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "doidi_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.migadu.com",
+   :domain               => "migadu.com",
+   :port                 => 587,
+   :user_name            => ENV['MIGADU_USERNAME'],
+   :password             => ENV['MIGADU_PASS'],
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
