ca3aa2e14ec2911683e88e740f6d12a48a460a91
quantd2 <dinhquan.tran@gmail.com>
2017-08-29 10:59:36 +0700

    admin edit item adding

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,4 +11,27 @@ class Admin::ItemsController < ApplicationController
     redirect_back fallback_location: :back, notice: "Xoá đồ thành công."
   end
 
+  def edit
+    @item = current_user.items.find params[:id]
+  end
+
+  def update
+    respond_to do |format|
+      if @item.update(item_params)
+        format.html {
+          redirect_to @item
+          flash[:notice] = 'Sửa thành công món đồ.' }
+        format.json { render :show, status: :ok, location: @item }
+      else
+        format.html { render :edit }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def item_params
+    params.require(:item).permit(:name, :description, :image, :category_id, :location_id)
+  end
+
 end
